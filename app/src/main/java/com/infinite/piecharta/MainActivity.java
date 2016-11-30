0e86853dd15f5e027969373b3f686ddb11a132b7
infinite <infinite.xu@gmail.com>
2016-11-30 10:55:58 +0800

    add text showed in center of the view

--- app/src/main/java/com/infinite/piecharta/MainActivity.java
+++ app/src/main/java/com/infinite/piecharta/MainActivity.java
@@ -8,19 +8,19 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private One one;
+    private PieChart one;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        one= (One) findViewById(R.id.one);
-        TestEntity entity=new TestEntity(30,"#FF7F50");
+        one= (PieChart) findViewById(R.id.one);
+        TestEntity entity=new TestEntity(50,"#FF7F50");
         TestEntity entity1=new TestEntity(50,"#DC143C");
-        TestEntity entity2=new TestEntity(70,"#00008B");
-        TestEntity entity3=new TestEntity(20,"#A9A9A9");
-        TestEntity entity4=new TestEntity(140,"#8B0000");
-        TestEntity entity5=new TestEntity(10,"#9400D3");
-        TestEntity entity6=new TestEntity(90,"#FFD700");
+        TestEntity entity2=new TestEntity(50,"#00008B");
+        TestEntity entity3=new TestEntity(50,"#A9A9A9");
+        TestEntity entity4=new TestEntity(50,"#8B0000");
+        TestEntity entity5=new TestEntity(50,"#9400D3");
+        TestEntity entity6=new TestEntity(50,"#FFD700");
 
         List<IPieElement> list=new ArrayList<>();
         list.add(entity);
@@ -31,5 +31,6 @@ public class MainActivity extends AppCompatActivity {
         list.add(entity5);
         list.add(entity6);
         one.setData(list);
+        one.setTitleText("年终总结比例图");
     }
 }
--- app/src/main/java/com/infinite/piecharta/PieChart.java
+++ app/src/main/java/com/infinite/piecharta/PieChart.java
@@ -7,8 +7,8 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
@@ -21,24 +21,54 @@ import java.util.List;
  * Created by inf on 2016/11/29.
  */
 
-public class One extends View{
+public class PieChart extends View{
 
+
+    /**
+     * view的宽高
+     */
     private int mWidth,mHeight;
-    private float mRadius;
+    /**
+     * 饼状图的半径、内部空白圆的半径
+     */
+    private float mRadius,mInnerRadius;
+    /**
+     * 饼状图的外切
+     */
     private RectF mPieRect;
+    /**
+     * 各种画笔
+     */
     private Paint mPiePaint,mBlankPaint,mLinePaint,mTextPaint;
+    /**
+     * 实体类集合
+     */
     private List<IPieElement> mElements;
+    /**
+     * 各个元素的角度
+     */
     private List<Float> mAngles=new ArrayList<>();
+    /**
+     * 元素的颜色
+     */
     private List<String> mColors=new ArrayList<>();
-    public One(Context context) {
+    /**
+     * 元素的占比
+     */
+    private List<String> mPercents=new ArrayList<>();
+    /**
+     * 中心文字
+     */
+    private String mText;
+    public PieChart(Context context) {
         this(context,null);
     }
 
-    public One(Context context, AttributeSet attrs) {
+    public PieChart(Context context, AttributeSet attrs) {
         this(context, attrs,0);
     }
 
-    public One(Context context, AttributeSet attrs, int defStyleAttr) {
+    public PieChart(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
     }
@@ -96,6 +126,8 @@ public class One extends View{
         mPieRect.right=mRadius;
         mPieRect.bottom=mRadius;
 
+        mInnerRadius= (float) (mRadius*0.6);
+
     }
 
     private Path mPath=new Path();
@@ -110,6 +142,7 @@ public class One extends View{
         canvas.drawArc(mPieRect,137,50,true,mPiePaint);
         //从12点方向开始画
         float sweepedAngle=-90;
+        mTextPaint.setTextSize(30);
         for(int i=0;mElements!=null&&mElements.size()>i;i++){
             //设置扇形的颜色
             mPiePaint.setColor(Color.parseColor(mColors.get(i)));
@@ -118,35 +151,83 @@ public class One extends View{
             canvas.drawArc(mPieRect,sweepedAngle,mAngles.get(i),true,mPiePaint);
             //扫过的角度++
             sweepedAngle+=mAngles.get(i);
+
+            String percentText=mPercents.get(i)+"%";
             //画分割线
             canvas.drawArc(mPieRect,sweepedAngle,1,true,mBlankPaint);
             sweepedAngle+=1;
             float x=getXCoordinate(mAngles.get(i),sweepedAngle);
             float y=getYCoordinate(mAngles.get(i),sweepedAngle);
-            Log.e("coor",x+"  "+y);
             mPath.reset();
             mPath.moveTo(x,y);
             mPath.lineTo((float)(x*1.2),(float)(y*1.2));
             canvas.drawPath(mPath,mLinePaint);
             mPath.reset();
             mPath.moveTo((float)(x*1.2),(float)(y*1.2));
-            float horizontalLineLength= getTextWidth(mTextPaint,String.valueOf(mAngles.get(i)));
+
+            //水平线的长度设置为文字长度的1.3倍
+            float horizontalLineLength= (float) (getTextWidth(mTextPaint, percentText)*1.3);
+            //当线的起点在第三、四象限时，先把path移动到终点位置，然后向起点画线，使后面画文字时，文字方向是正确的
             if (x<0){
                 horizontalLineLength=-horizontalLineLength;
-            }
-            mPath.lineTo((float)(x*1.2)+horizontalLineLength,(float)(y*1.2));
-            canvas.drawPath(mPath,mLinePaint);
-            float offsetV=20;
-            if (x>0){
-                offsetV=-getTextHeight(mTextPaint,String.valueOf(mAngles.get(i)));
+                mPath.moveTo((float)(x*1.2)+horizontalLineLength,(float)(y*1.2));
+                mPath.lineTo((float)(x*1.2),(float)(y*1.2));
             }else {
-                offsetV=40;
+                mPath.lineTo((float)(x*1.2)+horizontalLineLength,(float)(y*1.2));
+
             }
-            canvas.drawTextOnPath(String.valueOf(mAngles.get(i)),mPath,0,offsetV,mTextPaint);
-//            canvas.drawCircle(x,y,10,mLinePaint);
+            mPath.close();
+            canvas.drawPath(mPath,mLinePaint);
+            //垂直方向的偏移量，画文字时，文字显示在path的下方，为了让文字显示在上方，设置一个文字高度的垂直偏移量
+            float offsetV=-getTextHeight(mTextPaint,percentText);
+            canvas.drawTextOnPath(percentText,mPath,0,offsetV,mTextPaint);
+
+
+        }
+
+        //这里开始画中心空白部分以及文字，空白部分半径设置为整个圆半径的0.6倍
+
+        canvas.drawCircle(0, 0, mInnerRadius, mBlankPaint);
+        double index=Math.ceil(mText.length()/2)+1;
+        if (mText.length()%2==0){
+            index-=1;
+        }
+        String longerText=mText.substring(0, (int) index);
+        calculateTextPaint(longerText);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(longerText,0,0,mTextPaint);
+        canvas.drawText(mText.substring((int) (index)), 0, getTextHeight(mTextPaint,mText)+6, mTextPaint);
+    }
+
+    /**
+     * 把文字分两行，并画在圆内接正方形内，依此计算画笔的textSize
+     * @param text
+     */
+    private void calculateTextPaint(String text){
+        if (!TextUtils.isEmpty(text)){
+            measureText(text,100);
         }
     }
 
+    /**
+     * 递归调用，计算testSize
+     * @param text
+     * @param textSize
+     */
+    private void measureText(String text,int textSize){
+        mTextPaint.setTextSize(textSize);
+        float width=getTextWidth(mTextPaint,text);
+        float height=getTextHeight(mTextPaint,text);
+        if (width>mInnerRadius*1.41421){
+            textSize--;
+            measureText(text,textSize);
+            return;
+        }
+        if (height*2.5>mInnerRadius*1.41421){
+            textSize--;
+            measureText(text,textSize);
+        }
+    }
     private float getTextHeight(Paint paint,String text){
         Rect rect=new Rect();
         paint.getTextBounds(text,0,text.length(),rect);
@@ -154,7 +235,7 @@ public class One extends View{
     }
     private float getTextWidth(Paint paint,String text){
         Rect rect=new Rect();
-        paint.getTextBounds(text,0,text.length()-1,rect);
+        paint.getTextBounds(text,0,text.length(),rect);
         return rect.width();
     }
 
@@ -182,6 +263,17 @@ public class One extends View{
         invalidate();
     }
 
+    /**
+     * 设置中心文字
+     * @param text
+     */
+    public void setTitleText(String text){
+        mText=text;
+    }
+
+    /**
+     * 计算角度值和各个值的占比
+     */
     private void setValuesAndColors(){
         float sum=0;
         if (mElements!=null&&mElements.size()>0){
@@ -190,15 +282,16 @@ public class One extends View{
                 mColors.add(ele.getColor());
             }
             BigDecimal totleAngel=BigDecimal.valueOf(360-mElements.size());
-
             for(int i=0;i<mElements.size();i++){
                 IPieElement ele=mElements.get(i);
                 BigDecimal bigDecimal=new BigDecimal(String.valueOf(ele.getValue()));
                 BigDecimal sumBigDecimal=BigDecimal.valueOf(sum);
                 BigDecimal res=bigDecimal.divide(sumBigDecimal,5,BigDecimal.ROUND_HALF_UP);
+                //计算角度
                 BigDecimal angle=res.multiply(totleAngel);
                 mAngles.add(angle.floatValue());
-
+                //计算百分比保留两位小数并保存
+                mPercents.add(bigDecimal.multiply(new BigDecimal(100)).divide(sumBigDecimal,2,BigDecimal.ROUND_HALF_UP).toPlainString());
             }
         }
 
